7d8c7f8df2dd4749a35b90b875c6a0e1ca283525
Alec Kozakowski <AlecKozakowski@gmail.com>
2019-11-18 17:23:25 -0600

    Testing and fixes

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ import secrets
 #dbhost = os.environ.get('DBHOST')
 #dbname = os.environ.get('DBNAME')
 
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 #conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
 
 app = Flask(__name__)
-#app.config['SECRET_KEY']='SuperSecretKey'
-#app.config['SQLALCHEMY_DATABASE_URI'] = conn
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+app.config['SECRET_KEY']='SuperSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = conn
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
 
 db = SQLAlchemy(app)
 
