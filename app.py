9498aba889363d266efc797101c39f568f0508ba
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-11-30 14:31:19 -0600

    Updated app.py to include update/delete/search functionality

--- app.py
+++ app.py
@@ -1,126 +1,113 @@
 from flask import Flask
-from flask import render_template, redirect, request, flash
+from flask import render_template, redirect, request, flash, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms import IntegerField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_
 import pymysql
 import secrets
 #import os
+"""
+dbuser = os.environ.get('DBUSER')
+dbpass = os.environ.get('DBPASS')
+dbhost = os.environ.get('DBHOST')
+dbname = os.environ.get('DBNAME')
+"""
 
-#dbuser = os.environ.get('DBUSER')
-#dbpass = os.environ.get('DBPASS')
-#dbhost = os.environ.get('DBHOST')
-#dbname = os.environ.get('DBNAME')
 
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 #conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='SuperSecretKey'
+app.config['SECRET_KEY'] = 'SuperSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class group5_wbpl_materials(db.Model):
-    MaterialsID = db.Column(db.Integer, primary_key=True)
-    Title = db.Column(db.String(255))
-    Creator = db.Column(db.String(255))
-    YearCreated = db.Column(db.String(255))
-    Genre = db.Column(db.String(255))
-    MaterialType = db.Column(db.String(255))
-    Available = db.Column(db.Boolean(True))
-    DateAcquired = db.Column(db.Date())
-    LastModified = db.Column(db.Date())
+
+class ewestby_animalsapp(db.Model):
+    animalID = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(255))
+    Country = db.Column(db.String(255))
+    Weight = db.Column(db.Integer)
+    Quantity = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return "id: {0} | Name: {1} | Country: {2} | Weight: {3} | Quantity: {4}".format(self.id, self.iName, self.Country, self.Weight, self.Quantity)
 
 
-
-class MaterialsForm(FlaskForm):
-    Title = StringField('Title:', validators=[DataRequired()])
-    Creator = StringField('Creator:', validators=[DataRequired()])
-    YearCreated = StringField('Year Created:', validators=[DataRequired()])
-    Genre = StringField('Genre:', validators=[DataRequired()])
-    MaterialType = StringField('MaterialType:', validators=[DataRequired()])
-    Available = StringField('Available:', validators=[DataRequired()])
-    DateAcquired = StringField('Date Acquired:', validators=[DataRequired()])
-    LastModified = StringField('Last Modified:', validators=[DataRequired()])
+class AnimalForm(FlaskForm):
+    Name = StringField('Animal Name:', validators=[DataRequired()])
+    Country = StringField('Country:', validators = [DataRequired()])
+    Weight = IntegerField('Weight:', validators = [DataRequired()])
+    Quantity = IntegerField('Quantity:', validators = [DataRequired()])
+    
 
 @app.route('/')
 def index():
-    all_materials = group5_wbpl_materials.query.all()
-    return render_template('index.html', material=all_materials, pageTitle="materials")
-
-@app.route('/add_materials', methods=['GET','POST'])
-def add_materials():
-    form = MaterialsForm()
-    if form.validate_on_submit():
-        avl = 1
-        material = group5_wbpl_materials(Title=form.Title.data, Creator=form.Creator.data, YearCreated = form.YearCreated.data, Genre = form.Genre.data, MaterialType = form.MaterialType.data, Available = avl, DateAcquired=form.DateAcquired.data, LastModified=form.LastModified.data)
-        db.session.add(material)
-        db.session.commit()
-        return redirect('/')
-
-    return render_template('add_materials.html', form=form, pageTitle='Add materials')
+    all_animals = ewestby_animalsapp.query.all()
+    return render_template('index.html', animals = all_animals, pageTitle = 'Eric\'s Animals')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    if request.method == 'POST':
-        print('post method')
-        form = request.form
-        search_value = form['search_string']
-        print(search_value)
-        search = "%{0}%".format(search_value)
-        print(search)
-        results = group5_wbpl_materials.query.filter(or_(group5_wbpl_materials.Title.like(search), group5_wbpl_materials.Creator.like(search), group5_wbpl_materials.Genre.like(search))).all()
-        print(results)
-        return render_template('index.html', material = results, pageTitle='West Branch Public Library\'s materials', legend="Search Results")
-    else:
-        return redirect('/')
+        if request.method == 'POST':
+            form = request.form
+            search_value = form['search_string']
+            search = "%{0}%".format(search_value)
+            results = ewestby_animalsapp.query.filter(ewestby_animalsapp.Country.like(search)).all()
+            return render_template('index.html', animals = results, pageTitle = 'Eric\'s Animals', legend="Search Results")
+        else:
+            return redirect('/')
 
-@app.route('/delete_material/<int:MaterialsID>', methods=['GET','POST'])
-def delete_material(MaterialsID):
-    if request.method == 'POST': #if it's a POST request, delete the friend from the database
-        obj = group5_wbpl_materials.query.get_or_404(MaterialsID)
-        db.session.delete(obj)
+@app.route('/animal/new', methods =['GET', 'POST'])
+def add_animal():
+    form = AnimalForm()
+    if form.validate_on_submit():
+        animal = ewestby_animalsapp(Name = form.Name.data, Country = form.Country.data, Weight = form.Weight.data, Quantity = form.Quantity.data)
+        db.session.add(animal)
         db.session.commit()
-        flash('Material was successfully deleted!')
-        return redirect("/")
+        return redirect('/')
+    
+    return render_template('add_animal.html', form=form, pageTitle='Add a New Animal', legend="Add a New Animal")
 
+
+@app.route('/animal/<int:animal_id>', methods=['GET','POST'])
+def animal(animal_id):
+    animal = ewestby_animalsapp.query.get_or_404(animal_id)
+    return render_template('animal.html', form=animal, pageTitle='Animal Details')
+
+@app.route('/animal/<int:animal_id>/update', methods=['GET','POST'])
+def update_animal(animal_id):
+    animal = ewestby_animalsapp.query.get_or_404(animal_id)
+    form = AnimalForm()
+    if form.validate_on_submit():
+        animal.Name = form.Name.data
+        animal.Country = form.Country.data
+        animal.Weight = form.Weight.data
+        animal.Quantity = form.Quantity.data
+        db.session.commit()
+        flash('Your animal has been updated.')
+        return redirect(url_for('animal', animal_id=animal.animalID))
+    #elif request.method == 'GET':
+    form.Name.data = animal.Name
+    form.Country.data = animal.Country
+    form.Weight.data = animal.Weight
+    form.Quantity.data = animal.Quantity
+    return render_template('add_animal.html', form=form, pageTitle='Update Post',
+                            legend="Update An Animal")
+    
+@app.route('/animal/<int:animal_id>/delete', methods=['POST'])
+def delete_animal(animal_id):
+    if request.method == 'POST': #if it's a POST request, delete the friend from the database
+        animal = ewestby_animalsapp.query.get_or_404(animal_id)
+        db.session.delete(animal)
+        db.session.commit()
+        flash('Animal was successfully deleted!')
+        return redirect("/")
     else: #if it's a GET request, send them to the home page
         return redirect("/")
 
-@app.route('/materials/<int:MaterialsID>', methods=['GET', 'POST'])
-def get_material(MaterialsID):
-    material = group5_wbpl_materials.query.get_or_404(MaterialsID)
-    return render_template('materials.html', form=material, pageTitle = 'Materials')
-
-@app.route('/materials/<int:MaterialsID>/update', methods=['GET', 'POST'])
-def update_material(MaterialsID):
-    material = group5_wbpl_materials.query.get_or_404(MaterialsID)
-    form = MaterialsForm()
-    if form.validate_on_submit():
-        material.Title = form.Title.data
-        material.Creator = form.Creator.data
-        material.YearCreated = form.YearCreated.data
-        material.Genre = form.Genre.data
-        material.MaterialType = form.MaterialType.data
-        material.Available = form.Available.data
-        material.DateAcquired = form.DateAcquired.data
-        material.LastModified = form.LastModified.data
-        return redirect(url_for('get_material', MaterialsID = MaterialsID))
-    form.Title.data = material.Title
-    form.Creator.data = material.Creator
-    form.YearCreated.data = material.YearCreated
-    form.Genre.data = material.Genre
-    form.MaterialType.data = material.MaterialType
-    form.Available.data = material.Available
-    form.DateAcquired.data = material.DateAcquired
-    form.LastModified.data = material.LastModified
-    return render_template('update_materials.html', form = form, pageTitle='Updated Materials')
-
 
 if __name__ == '__main__':
-    app.run(debug==False)
+    app.run(debug=True)
