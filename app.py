b5f02fadce9ecb07bf71518d4dc9cf815ebe2938
NorwegianSchnitzel <eric-westby@uiowa.edu>
2019-12-02 22:19:03 -0600

    Created link to Circulations page, working on check-out check-in function next

--- app.py
+++ app.py
@@ -70,6 +70,18 @@ class group5_wbpl_patrons(db.Model):
     def __repr__(self):
         return "id: {0} | First Name: {1} | Last Name: {2} | Email: {3} | Phone: {4} | Address: {5} | City: {6} | State: {7} | Zipcode: {8} | Birthdate: {9} | Created: {10}".format(self.patron_id, self.First_Name, self.Last_Name, self.Email, self.Phone, self.Address, self.City, self.State, self.Zipcode, self.Birthdate, self.created_at)
 
+class group5_wbpl_circulation(db.Model):
+    circulation_id = db.Column(db.Integer, primary_key=True)
+    patron_id = db.Column(db.Integer, foreign_key=True)
+    MaterialsID = db.Column(db.Integer, foreign_key=True)
+    checked_out = db.Column(db.Boolean)
+    title = db.Column(db.String)
+    material_type = db.Column(db.String)
+    checkout_date = db.Column(db.Date)
+    due_date = db.Column(db.Date)
+    
+    def __repr__(self):
+        return "CirculationID: {0} | PatronID: {1} | MaterialID: {2} | Checked Out: {3} | Title: {4} | Material Type: {5} | Checkout Date: {6} | Due Date: {7}".format(self.circulation_id, self.patron_id, self.MaterialsID, self.checked_out, self.title, self.material_type, self.checkout_date, self.due_date)
 """
 class MaterialForm(FlaskForm):
     Title = StringField('Title:', validators = [DataRequired()])
@@ -109,6 +121,12 @@ class PatronForm(FlaskForm):
 def index():
     return render_template('index.html', pageTitle = 'West Branch Public Library')
 
+
+@app.route('/AllMaterials', methods=['GET', 'POST'])
+def AllMaterials():
+    all_materials = group5_wbpl_materials.query.all()
+    return render_template('AllMaterials.html', materials = all_materials, pageTitle = 'West Branch Public Library Catalogue')
+
 @app.route('/searchMaterials', methods=['GET', 'POST'])
 def searchMaterials():
         if request.method == 'POST':
@@ -119,11 +137,6 @@ def searchMaterials():
             return render_template('AllMaterials.html', materials = results, pageTitle = 'West Branch Public Library Catalogue', legend="Search Results")
         else:
             return redirect('/')
-
-@app.route('/AllMaterials', methods=['GET', 'POST'])
-def AllMaterials():
-    all_materials = group5_wbpl_materials.query.all()
-    return render_template('AllMaterials.html', materials = all_materials, pageTitle = 'West Branch Public Library Catalogue')
 
 @app.route('/material/new', methods =['GET', 'POST'])
 def add_materials():
@@ -157,7 +170,7 @@ def update_material(material_id):
         material.LastModified = form.LastModified.data
         db.session.commit()
         flash('Your material has been updated.')
-        return redirect(url_for('material', material_id=material.MaterialsID))
+        return redirect(url_for('AllMaterials', material_id=material.MaterialsID))
     #elif request.method == 'GET':
     form.Title.data = material.Title
     form.Creator.data = material.Creator
@@ -177,9 +190,15 @@ def delete_material(material_id):
         db.session.delete(material)
         db.session.commit()
         flash('Material was successfully deleted!')
-        return redirect("/")
+        return redirect("/AllMaterials")
     else: #if it's a GET request, send them to the home page
-        return redirect("/")
+        return redirect("/AllMaterials")
+
+
+@app.route('/AllPatrons', methods=['GET', 'POST'])
+def AllPatrons():
+    all_patrons = group5_wbpl_patrons.query.all()
+    return render_template('AllPatrons.html', patrons = all_patrons, pageTitle = 'West Branch Public Library Patrons')
 
 @app.route('/searchPatrons', methods=['GET', 'POST'])
 def searchPatrons():
@@ -192,15 +211,60 @@ def searchPatrons():
         else:
             return redirect('/')
 
-@app.route('/AllPatrons', methods=['GET', 'POST'])
-def AllPatrons():
-    all_patrons = group5_wbpl_patrons.query.all()
-    return render_template('AllPatrons.html', patrons = all_patrons, pageTitle = 'West Branch Public Library Patrons')
-
 @app.route('/patron/<int:patron_id>', methods=['GET','POST'])
 def patron(patron_id):
     patron = group5_wbpl_patrons.query.get_or_404(patron_id)
     return render_template('patron.html', form=patron, pageTitle='Patron Details')
+
+@app.route('/patron/<int:material_id>/update', methods=['GET','POST'])
+def update_patron(patron_id):
+    patron = group5_wbpl_patrons.query.get_or_404(patron_id)
+    form = PatronForm()
+    if form.validate_on_submit():
+        patron.First_Name = form.First_Name.data
+        patron.Last_Name = form.Last_Name.data
+        patron.Email = form.Email.data
+        patron.Phone = form.Phone.data
+        patron.Address = form.Address.data
+        patron.City = form.City.data
+        patron.State = form.State.data
+        patron.Zipcode = form.Zipcode.data
+        patron.Birthdate = form.Birthdate.data
+        patron.created_at = form.created_at.data
+        db.session.commit()
+        flash('Patron has been updated.')
+        return redirect(url_for('AllPatrons', patron_id=patron.patron_id))
+    #elif request.method == 'GET':
+    form.First_Name.data = patron.First_Name
+    form.Last_Name.data = patron.Last_Name
+    form.Email.data = patron.Email
+    form.Phone.data = patron.Phone
+    form.Address.data = patron.Address
+    form.City.data = patron.City
+    form.State.data = patron.State
+    form.Zipcode.data = patron.Zipcode
+    form.Birthdate.data = patron.Birthdate
+    form.created_at.data = patron.created_at
+    return render_template('add_patrons.html', form=form, pageTitle='Update Patron',
+                            legend="Update A Patron")
+
+
+@app.route('/patron/<int:patron_id>/delete', methods=['POST'])
+def delete_patron(patron_id):
+    if request.method == 'POST': #if it's a POST request, delete the friend from the database
+        patron = group5_wbpl_patrons.query.get_or_404(patron_id)
+        db.session.delete(patron)
+        db.session.commit()
+        flash('Patron was successfully deleted!')
+        return redirect("/AllPatrons")
+    else: #if it's a GET request, send them to the home page
+        return redirect("/AllPatrons")
+    
+@app.route('/AllCirculations', methods=['GET', 'POST'])
+def AllCirculations():
+    all_circulations = group5_wbpl_circulation.query.all()
+    return render_template('AllCirculations.html', circulations = all_circulations, pageTitle = 'West Branch Public Library Circulation')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
